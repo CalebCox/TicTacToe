6a528027c2a6205e608654b9afc0b257bebb02cb
Caleb Cox <caleb@caleb-cox.com>
2017-09-02 22:24:18 -0400

    Updated stylesheet to create board layout, updated HTML, added jQuery.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    alert("jQuery here!");
+});
+
+
+
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+// enable game board once player has chosen X's or O'x disable operations.
+// make sure AI is the correct piece (X or O).
+// when space is clicked, check if available. If it is available
+// Check if surrounding spaces are matched or not. If it is not available, do nothing.
+
+
